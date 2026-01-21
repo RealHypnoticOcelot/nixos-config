bee22274c052cb64539867a5b8a2227e8392b690
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 14:02:57 -0500

    fixed it, maybe

--- modules/disko/btrfs-encrypted.nix
+++ modules/disko/btrfs-encrypted.nix
@@ -1,67 +1,70 @@
-{ hostName, inputs, ... }:
+{ hostName, config, ... }:
 
 {
-  inputs.disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = { # ESP Partition, for UEFI bootloaders
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "512M"; # A little overkill, but good to be safe!
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = { # The rest of the partition, contains all of the volumes you'll need
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "root_vg_${hostName}";
-                settings = {
-                  allowDiscards = true;
-                  # Allows for TRIM requests, where the operating system can communicate with the disk to determine what sectors can be discarded
-                };
+  config = {
+    boot.supportedFilesystems = ["btrfs"];
+    disko.devices = {
+      disk = {
+        main = {
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = { # ESP Partition, for UEFI bootloaders
+                priority = 1;
+                name = "ESP";
+                start = "1M";
+                end = "512M"; # A little overkill, but good to be safe!
+                type = "EF00";
                 content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                    # /root is the primary volume where the operating system goes!
-                      mountpoint = "/";
-                      mountOptions = [
-                        "compress=zstd" # Compresses easily compressable files(like text) using the zstd compression algorithm. Minor effect on the CPU, but saves plenty of space.
-                        "noatime" # Disables recording the last time a file was accessed
-                        "subvol=root" # Mount into the root volume; this one is technically redundant, but the other two aren't. That'll ensure they mount to /persistent instead of /root/persistent, for example.
-                      ];
-                    };
-                    "/persistent" = {
-                    # /persistent is a volume specific to Impermanence, and it's where your persistent files go.
-                    # Everything in here gets bound to the corresponding location in /root upon boot, and extraneous files get purged.
-                    # (If you have Impermanence disabled, this will just be empty, and will not affect anything)
-                      mountpoint = "/persistent";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                        "subvol=persistent"
-                      ];
-                    };
-                    "/nix" = {
-                    # /nix stores everything Nix related, like packages, build outputs, all that fancy stuff. It's what allows for rollbacks!
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                        "subvol=nix"
-                      ];
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              root = { # The rest of the partition, contains all of the volumes you'll need
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "root_vg_${hostName}";
+                  settings = {
+                    allowDiscards = true;
+                    # Allows for TRIM requests, where the operating system can communicate with the disk to determine what sectors can be discarded
+                  };
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/root" = {
+                      # /root is the primary volume where the operating system goes!
+                        mountpoint = "/";
+                        mountOptions = [
+                          "compress=zstd" # Compresses easily compressable files(like text) using the zstd compression algorithm. Minor effect on the CPU, but saves plenty of space.
+                          "noatime" # Disables recording the last time a file was accessed
+                          "subvol=root" # Mount into the root volume; this one is technically redundant, but the other two aren't. That'll ensure they mount to /persistent instead of /root/persistent, for example.
+                        ];
+                      };
+                      "/persistent" = {
+                      # /persistent is a volume specific to Impermanence, and it's where your persistent files go.
+                      # Everything in here gets bound to the corresponding location in /root upon boot, and extraneous files get purged.
+                      # (If you have Impermanence disabled, this will just be empty, and will not affect anything)
+                        mountpoint = "/persistent";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                          "subvol=persistent"
+                        ];
+                      };
+                      "/nix" = {
+                      # /nix stores everything Nix related, like packages, build outputs, all that fancy stuff. It's what allows for rollbacks!
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                          "subvol=nix"
+                        ];
+                      };
                     };
                   };
                 };
