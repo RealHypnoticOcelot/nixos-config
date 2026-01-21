9f4b50a9c7352532bc790c5dfdae5611fed960e1
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 13:15:34 -0500

    Doesn't really fix my current issue, but we'd like for Impermanence to work

--- modules/impermanence/mkpersist.nix
+++ modules/impermanence/mkpersist.nix
@@ -53,4 +53,34 @@ in
   };
   users.mutableUsers = lib.mkForce false;
   users.users.${userName}.hashedPasswordFile = "/persistent/passwd_${userName}";
+
+  fileSystems = {
+    "/persistent".neededForBoot = true;
+    "/nix".neededForBoot = true;
+  };
+
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/mapper/root_vg_${hostName} /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/persistent/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/persistent/old_roots/$timestamp/"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/persistent/old_roots/ -mindepth 1 -maxdepth 1 -mtime +1); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 }
