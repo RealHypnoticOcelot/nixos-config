d237d712ed8059a63adc37cb236a1f23f9cf393c
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 20:28:20 -0500

    Lots of stuff involved in partitioning!

--- hosts/disko-partition/configuration.nix
+++ hosts/disko-partition/configuration.nix
@@ -1,4 +1,15 @@
-{ stateVersion, ... }:
+{ stateVersion, userName, ... }:
 {
+
+  users.mutableUsers = lib.mkDefault true; # This gets overridden if you use Impermanence
+  users.users.${userName} = {
+    isNormalUser = true;
+    description = userName;
+    extraGroups = [
+      "wheel" # Allows for use of sudo
+      "video" # Allows for access to video devices(and virtual webcams, like OBS)
+    ];
+  };
+
   system.stateVersion = stateVersion;
 }
--- hosts/disko-partition/home.nix
+++ hosts/disko-partition/home.nix
@@ -1,9 +1,13 @@
-{ stateVersion, ... }:
+{ stateVersion, userName, ... }:
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   home = {
+    username = userName;
+
+    homeDirectory = "/home/${userName}";
+
     inherit stateVersion;
   };
 }
