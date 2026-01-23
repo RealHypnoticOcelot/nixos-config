9f98a10f166dd6dfde99fd422ca49653be2b809c
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-23 01:33:13 -0500

    let's see if i can get apps launching!

--- modules/common/window-system/hyprland/home-manager.nix
+++ modules/common/window-system/hyprland/home-manager.nix
@@ -7,8 +7,28 @@
     settings = {
       "$mod" = "SUPER";
       # Startup Apps
-      exec-once = [
-        "waybar"
+      exec-once = [];
+      bind = [
+        "$mod, space, exec, hyprlauncher"
+      ];
+      binde = [ # Bind something that'll repeat if you hold it
+        # xf86bluetooth
+        # xf86keyboard
+        # xf86tools
+        # xf86audiomicmute
+        # xf86display
+        # xf86wlan
+        # xf86messenger
+        # xf86go
+        # cancel
+        # xf86favorites
+        # xf86audiomute
+        # xf86audiolowervolume
+        # "SHIFT, xf86audiolowervolume
+        # xf86audioraisevolume
+        # "SHIFT, xf86audioraisevolume
+        ", xf86monbrightnessdown, exec, brightnessctl s 5%-"
+        ", xf86monbrightnessup, exec, brightnessctl s +5%"
       ];
     };
   };
