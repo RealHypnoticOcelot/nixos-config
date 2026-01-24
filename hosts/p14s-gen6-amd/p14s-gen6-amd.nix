e02da1e9fabb88dc391a9e0520f911cd6287f96a
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-23 20:41:13 -0500

    fix mute microphone button!

--- hosts/p14s-gen6-amd/p14s-gen6-amd.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd.nix
@@ -16,4 +16,11 @@
   environment.systemPackages = with pkgs; [
     alsa-utils
   ];
+  systemd.services.alsaMicrophone = { # Creates systemd service that disables Auto-Mute Mode on the microphone on sound card 1, automatically on boot
+    enable = true;
+    script = ''
+      amixer -c 1 set "Auto-Mute Mode" Disabled
+    '';
+    wantedBy = [ "multi-user.target" ];
+  };
 }
