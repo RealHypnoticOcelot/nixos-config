bed60768d3d496405a6f4ae86f9f0ddd64164a88
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-23 21:32:39 -0500

    I'm a GENIUS!

--- hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
@@ -11,7 +11,7 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
-      binde = [ # Bind something that'll repeat if you hold it
+      bindr = [ # Bind something upon key release
         ", xf86audiomicmute, exec, brightnessctl -d 'platform::micmute' s $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q MUTED && echo 0 || echo 1)"
         # Toggle the microphone LED to match the state of microphone mute
       ];
