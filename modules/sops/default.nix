78aa6c8ad41cee0ba8475f726375ce6e79c4cef4
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-25 23:04:32 -0500

    hrrmmm

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -4,9 +4,4 @@
     sops
     age
   ];
-  sops = {
-    defaultSopsFile = ../../secrets/secrets.yaml; # Secrets encrypted with the public key
-    defaultSopsFormat = "yaml";
-    age.keyFile = "/home/${userName}/.config/sops/age/keys.txt";
-  };
 }
--- modules/sops/home-manager.nix
+++ modules/sops/home-manager.nix
@@ -4,5 +4,8 @@
     defaultSopsFile = ../../secrets/secrets.yaml; # Secrets encrypted with the public key
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${userName}/.config/sops/age/keys.txt";
+    secrets = {
+      git_email = {};
+    }
   };
 }
