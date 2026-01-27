648bb4e7e2e14abfa35f705fde7518a8481ac833
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-27 17:46:01 -0500

    weird, still captchas! maybe it just needs to be GET instead of POST?

--- apps/librewolf/home-manager.nix
+++ apps/librewolf/home-manager.nix
@@ -16,8 +16,9 @@
           {
             Name = "Startpage";
             Description = "Startpage Search";
-            URLTemplate = "https://www.startpage.com/sp/search?query={searchTerms}";
-            Method = "POST";
+            URLTemplate = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=d05529b22695d9b21ff0bfe3d59b0435c473b251863471092ba162b9a294373c050a20e9412164b63e1334d4b1fc2d4580f9085c3c08441b6809c72c78d039363453dbf0f832bdceebc83f234de3";
+            # REALLY long, but this contains user preferences! Now, when we search, theme and everything will always be set by default.
+            Method = "GET";
             IconURL = "https://www.startpage.com/favicon.ico";
             SuggestURLTemplate = "https://www.startpage.com/osuggestions?q={searchTerms}";
           }
--- apps/thunderbird/home-manager.nix
+++ apps/thunderbird/home-manager.nix
@@ -19,6 +19,8 @@
                 template = "https://www.startpage.com/sp/search";
                 params = {
                   { name = "query"; value = "{searchTerms}"; }
+                  { name = "prfe"; value = "d05529b22695d9b21ff0bfe3d59b0435c473b251863471092ba162b9a294373c050a20e9412164b63e1334d4b1fc2d4580f9085c3c08441b6809c72c78d039363453dbf0f832bdceebc83f234de3"; }
+                  # REALLY long, but this contains user preferences! Now, when we search, theme and everything will always be set by default.
                 };
                 icon = "https://www.startpage.com/favicon.ico";
                 method = "POST";
