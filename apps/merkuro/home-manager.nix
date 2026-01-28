b30f70e7e37bfcb8cfa2f3a912c6c75ca5b8a9fc
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 18:53:17 -0500

    clean this up! also contributed this solution to the related kde-pim system package with nixpkgs pr #484899

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -3,11 +3,9 @@
 {
   home.packages = with pkgs; [
     kdePackages.merkuro
+  ]
+  ++ lib.optionals config.services.desktopManager.plasma6.enable [ # Packages needed for the Contacts widget on Plasma 6 to work properly
     kdePackages.kcontacts
     kdePackages.akonadi-contacts
   ];
-  # programs.kde-pim = {
-  #   enable = true;
-  #   merkuro = true;
-  # };
 }
